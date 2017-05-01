df3bebb95b8ea915220ed3a3412095d97a04d0a5
Jason Norwood-Young <jason@10layer.com>
2017-05-01 15:49:42 +0200

    Hand off most of the intelligence to the State library

--- app/initialize.js
+++ app/initialize.js
@@ -1,56 +1,20 @@
 var _ = require("lodash");
-var ImgHandler = require("./libs/imghandler");
+
 var eventhandler = require("./libs/eventhandler");
 var ArrowDrawer = require("./libs/arrowdrawer");
-var API = require("./libs/api");
 var $ = require("jquery");
+var State = require("./libs/state");
 
-var imghandler = new ImgHandler();
-var currentId = null;
-
-document.addEventListener('DOMContentLoaded', function() {
+$(function() {
   // do your setup here
-	console.log('Initialized app');
-	eventhandler.listen("img-loaded", (params) => {
-		console.log(params);
-		var el = $("<canvas id='drawingLayer'>");
-		el[0].width = params.width;
-		el[0].height = params.height;
-		$("#businesstime").append(el);
-		if (!params.id) {
-			API.save({
-				img: params.src
-			});
-		}
-		ArrowDrawer();
-	});
-	ImgHandler({ el: "#businesstime" });
-	eventhandler.listen("saved", params => {
-		console.log(params);
-		$("#code").val(params.id);
-	});
-
-	var loadId = () => {
-		var id = $("#code").val();
-		if ((id.length >= 8) && (id != currentId)) {
-			API.get(id)
-			.then(result => {
-				currentId = id;
-				imghandler.populateImg(result.result.img, id);
-				location.hash = "#" + id;
-			})
-			.catch(err => {
-				console.error(err);
-			});
-		}
-  	};
+  	var state = new State();
 
 	$("#code").on("change", e => {
-		loadId();
+		state.loadId();
 	});
 
 	$("#code").on("keyup", e => {
-		loadId();
+		state.loadId();
   	});
 
 	var checkHash = () => {
@@ -58,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			var code = location.hash.substr(1);
 			console.log("Checking code", code);
 			$("#code").val(code);
-			loadId();
+			state.loadId();
 		}
 	};
 
